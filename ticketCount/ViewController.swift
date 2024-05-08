6952f9eefe1fc40fd38fc0219783fbdb66f6d90b
emioiso <jijjjiijiijijiji@gmail.com>
2024-05-08 15:51:24 +0900

    Initial Commit

---
+++ ticketCount/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ticketCount
+//
+//  Created by emi oiso on 2024/05/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
