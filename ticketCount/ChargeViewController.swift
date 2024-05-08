71267ba7f60520ac41b1618c7beacbab1a4723a4
emioiso <jijjjiijiijijiji@gmail.com>
2024-05-08 16:40:52 +0900

    HomeViewControllerのPresentationをフルスクリーンにしました。

---
+++ ticketCount/ChargeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ChargeViewController.swift
+//  ticketCount
+//
+//  Created by emi oiso on 2024/05/08.
+//
+
+import UIKit
+
+class ChargeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
