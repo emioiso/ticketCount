b8a94f035d3a565ba126c8cd5bf15da89d20c968
emioiso <jijjjiijiijijiji@gmail.com>
2024-05-11 17:14:29 +0900

    ChargeViewControllerのtextFieldの入力表示の出力を黒丸にしました。

--- ticketCount/ChargeViewController.swift
+++ ticketCount/ChargeViewController.swift
@@ -10,13 +10,17 @@ import UIKit
 class ChargeViewController: UIViewController {
 
     @IBOutlet weak var chargeLabel: UILabel!
-    
+    //textFieldの
+    @IBOutlet weak var textField: UITextField!
     // チャージ額（または他の値）を保持する変数
     var currentCharge: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // パスワード入力を黒丸で表示
+        textField.isSecureTextEntry = true
+                
         // デフォルト値でラベルを初期化
         chargeLabel.text = "\(currentCharge)"
     }
